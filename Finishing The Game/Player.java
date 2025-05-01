dfeb4b8e7c64b04f37da62258642a2ea80194571
OvulatingJackRabbit <emanfosho21@gmail.com>
2025-05-01 18:20:38 -0400

    Create Player.java

---
+++ Finishing The Game/Player.java
@@ -0,0 +1,27 @@
+public class Player {
+    private String name;
+    private Grid board;
+
+    public Player(String name) {
+        this.name = name;
+        this.board = new Grid();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grid getBoard() {
+        return board;
+    }
+
+    public void placeShip(Ship ship, int row, int col, int direction) {
+        ship.setLocation(row, col);
+        ship.setDirection(direction);
+        board.placeShip(ship);
+    }
+
+    public void printBoard() {
+        board.printBoard();
+    }
+}
